f0bca3f9282904dab8b9f497469fed77ce7538da
Julian Martinez <jdgm_1993@yahoo.com>
2018-01-23 14:35:47 -0500

    cambio de title en contacto
    Se ajusto acorde al uso del documento contacto.php

--- contacto.php
+++ contacto.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Contacto</title>
+	<title>Especificación de nomina</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="style/estilos.css">
